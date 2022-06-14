ce7d3391d99d29db539f04ab1edb925e09dc5634
raghadalharbi396 <raghad.alharbi396@gmail.com>
2022-06-14 21:25:33 +0300

    first adding

---
+++ animals.js
@@ -0,0 +1,13 @@
+ class animals{
+    name ;
+     type ;
+constructor(n,t){
+    this.name = n;
+    this.type = t;
+}
+
+callname(){
+    return this.name;
+}
+}
+export default animals;
---
+++ app.js
@@ -0,0 +1,16 @@
+import animals from './animals.js';
+
+console.log("Hallo wolde");
+var num = 10;
+for(var i = 0; i<10 ; i++){
+    if(i%2 == 0)
+    num++;
+}
+
+console.log(num);
+
+var n1 =  new animals("sun","cat");
+console.log(n1);
+
+console.log(n1.callname());
+git 
